9277c30a52c5ec6de4e5cb6d0ae1995867bfd27f
Mark Anthony Viola <mark.anthony.viola@gmail.com>
2015-05-26 00:48:53 -0400

    HTML/CSS + Delete & Edit Functionaility
    Added HTML and CSS (Default). Ruby code to allow users to delete and
    edit articles

---
+++ 5-ruby-on-rails-blogger/app/controllers/articles_controller.rb
@@ -0,0 +1,39 @@
+class ArticlesController < ApplicationController
+	include ArticlesHelper
+
+	def index 
+		@articles = Article.all
+	end
+
+	def show
+		@article = Article.find(params[:id])
+	end
+
+	def new
+		@article = Article.new
+	end
+
+	def create
+  		@article = Article.new(article_params)
+  		@article.save
+  		redirect_to article_path(@article)
+	end
+
+	def destroy
+  		@article = Article.find(params[:id])
+  		@article.destroy
+  		redirect_to articles_path
+	end
+
+	def edit
+  		@article = Article.find(params[:id])
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		@article.update(article_params)
+
+		flash.notice = "Article '#{@article.title}' Updated!"
+		redirect_to article_path(@article)
+	end
+end
