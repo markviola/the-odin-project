a2fd1d69cd4a79d5378b5fe2a15f5bd9035f2f23
Mark Anthony Viola <mark.anthony.viola@gmail.com>
2015-05-26 15:42:58 -0400

    Allow User to Add Pictures
    In the edit/create article section, users can now add images to articles

--- 5-ruby-on-rails-blogger/db/schema.rb
+++ 5-ruby-on-rails-blogger/db/schema.rb
@@ -11,13 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150526142636) do
+ActiveRecord::Schema.define(version: 20150526184117) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "comments", force: :cascade do |t|
