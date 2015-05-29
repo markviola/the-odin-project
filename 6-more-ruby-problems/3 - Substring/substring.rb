0edcf172121a332baee18405f024e36dc85f629e
Mark Anthony Viola <mark.anthony.viola@gmail.com>
2015-05-29 00:49:17 -0400

    Completed More Ruby Problems
    Completed the third Ruby problem

--- 6-more-ruby-problems/3 - Substring/substring.rb
+++ 6-more-ruby-problems/3 - Substring/substring.rb
@@ -1,8 +1,23 @@
 def substring(words, dict)
-	all_words = words.split
-
+	words = words.downcase
+	ret_hash = {}
+	for x in 0...dict.length
+		if words.include?(dict[x])
+			#Check if entire value of 'words' is equal to a 'dict' element
+			if (words == dict[x])
+				ret_hash[dict[x]] = 1
+			#Check if last part of 'words' is the searched 'dict' element
+			elsif (words.reverse[0...dict[x].length] == dict[x].reverse)
+				ret_hash[dict[x]] = words.split(dict[x]).length
+			#Not an edge case
+			else
+				ret_hash[dict[x]] = words.split(dict[x]).length - 1
+			end
+		end
+	end
+	return ret_hash
 end
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-print substring("below", dictionary)
-print substring("Howdy partner, sit down! How's it going?", dictionary)
+puts substring("below", dictionary)
+puts substring("Howdy partner, sit down! How's it going?", dictionary)
